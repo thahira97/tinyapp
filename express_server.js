b33c67d0a22aab093a3d968ae95181d81e241a0c
thahira97 <thahirasheerin3@gmail.com>
2023-01-05 17:28:45 -0500

    Add short URL id and Long URL id

--- express_server.js
+++ express_server.js
@@ -10,16 +10,22 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/:id", (req, res) => {
+
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
