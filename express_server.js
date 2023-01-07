6a9e01d925289cd45740be311dedb30250ec28b6
thahira97 <thahirasheerin3@gmail.com>
2023-01-06 20:26:06 -0500

    Redirecting the short URL to LongURL homepage

--- express_server.js
+++ express_server.js
@@ -7,28 +7,37 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 const generateRandomString = function () {
-  return Math.random().toString(36).substring(2, 8)
-}
+  return Math.random().toString(36).substring(2, 8);
+};
 
 app.set("view engine", "ejs");
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+/// GET Request
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+//// GET Request to enter longURL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
+////Get Request to read the Particular id
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
@@ -36,10 +45,21 @@ app.get("/urls/:id", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
-app.post("/urls", (req, res)=>{
-  console.log(req.body)
-  res.send("OK")
-})
+
+/// POST Request for the submit form to get the short url
+app.post("/urls", (req, res) => {
+  const id = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[id] = longURL;
+  // console.log(req.body)
+  res.redirect(`urls/${id}`);
+});
+// Redirecting the shortURL link to longURL page
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
